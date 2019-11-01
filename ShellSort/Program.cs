c5e71209f62e7054dc508e9844764fde85ad9528
Ramazan KILIMCI <rkilimci@borusan.com>
2019-11-01 07:47:26 +0300

    New ShellSort algorithm was added

---
+++ ShellSort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ShellSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, 2, 10 };
+
+            ShellSort ob = new ShellSort();
+            ob.Sort(arr);
+            ob.PrintArray(arr);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ShellSort/ShellSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShellSort
+{
+    class ShellSort
+    {
+        //Prints any arrays
+        public void PrintArray(int[] arr)
+        {
+
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+                Console.Write(arr[i] + " \n");
+            Console.ReadLine();
+        }
+
+        //Sorting array items starting from arr[0]
+        //Always compares all next array items. For n-th item, it will always compare arr[n+1], arr[n+2], arr[n+3]
+        // If arr[n] > arr[n+1], order of items will be switched
+        public int Sort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
